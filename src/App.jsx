670b29c70e2da75f66dc073e52cbdcdcefacbfe7
Tristan <tristanmikie@gmail.com>
2025-10-31 11:28:30 -0400

    third commit with tailwind initialized and routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import Landing from './Landing.jsx'
+import {Routes, Route, Link} from 'react-router-dom'
+import Landing from './pages/Landing.jsx'
 function App() {
 
   return (
     <>
-      <Landing />
+      <header>
+        <h1>Rick and Morty Webapp</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/characters/page/1">Characters</Link>
+          <Link to="locations/page/1">Locations</Link>
+          <Link to="/episodes/page/1">Episodes</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+
+        {/*Characters pages*/}
+        {/*Locations pages*/}
+        {/*Episodes pages*/}
+        {/*404 page*/}
+
+      </Routes>
+     
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
